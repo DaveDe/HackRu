330e7407a7c9faa90f69d72cfaf14592a378a30d
David DeFazio <davedefazio1@gmail.com>
2016-04-16 17:26:52 -0400

    send login info to server

--- app/src/main/java/com/example/david/hackathon/Login.java
+++ app/src/main/java/com/example/david/hackathon/Login.java
@@ -9,25 +9,87 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class Login extends AppCompatActivity {
 
-    private Button login;
+    public final String serverURL = "";
+
+    private EditText getUsername;
+    private EditText getPassword;
+    private Button loginButton;
+
+    private String jResponse;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login);
-        login = (Button) findViewById(R.id.login);
-        login.setOnClickListener(new View.OnClickListener() {
+
+        getUsername = (EditText) findViewById(R.id.username);
+        getPassword = (EditText) findViewById(R.id.password);
+        loginButton = (Button) findViewById(R.id.login_button);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                String username = getUsername.getText().toString();
+                String password = getPassword.getText().toString();
+                sendAndRecievePostRequest(username,password);
+                Toast.makeText(getBaseContext(),jResponse,Toast.LENGTH_LONG).show();
+                //check jresponse before going to MainActivity
                 Intent i = new Intent(getBaseContext(),MainActivity.class);
                 startActivity(i);
             }
         });
 
     }
+
+    private void sendAndRecievePostRequest(String username, String password){
+
+        // Instantiate the RequestQueue.
+        RequestQueue queue = Volley.newRequestQueue(this);
+        String url ="http://"+serverURL+"/login";
+        JSONObject jsonBody = new JSONObject();
+        try{
+            jsonBody.put("Username: ", username);
+            jsonBody.put("password", password);
+        }catch (JSONException j){}
+        final String mRequestBody = jsonBody.toString();
+
+        JsonObjectRequest jsObjRequest = new JsonObjectRequest
+                (Request.Method.POST, url, mRequestBody,new Response.Listener<JSONObject>() {
+
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        jResponse = "RESPONSE: " + response.toString();
+                        //Toast.makeText(getBaseContext(),response.toString(),Toast.LENGTH_LONG).show();
+                    }
+                }, new Response.ErrorListener() {
+
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        jResponse = "ERROR: " + error.toString();
+                    }
+                });
+
+        queue.add(jsObjRequest);
+
+    }
+
 
 }
