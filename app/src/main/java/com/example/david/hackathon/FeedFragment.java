c967419236cf10f794327b22e3165e4069094af5
Mike Botti <mikexbotti@gmail.com>
2016-04-16 21:56:23 -0400

    recycleview

--- app/src/main/java/com/example/david/hackathon/FeedFragment.java
+++ app/src/main/java/com/example/david/hackathon/FeedFragment.java
@@ -2,11 +2,23 @@ package com.example.david.hackathon;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class FeedFragment extends Fragment {
+
+    private RecyclerView rv;
+    private List<PostInfo> postList = new ArrayList<PostInfo>();
+    private PostAdapter pAdapter;
+
 
     public FeedFragment() {
         // Required empty public constructor
@@ -21,7 +33,33 @@ public class FeedFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.feed, container, false);
+        super.onCreate(savedInstanceState);
+        Toolbar toolbar = (Toolbar) view.findViewById(R.id.toolbar);
+        //setSupportActionBar(toolbar);
+
+
+        pAdapter = new PostAdapter(postList);
+        LinearLayoutManager llm = new LinearLayoutManager(getContext());
+        rv = (RecyclerView)view.findViewById(R.id.rv);
+        rv.setLayoutManager(llm);
+        rv.setItemAnimator(new DefaultItemAnimator());
+        rv.setAdapter(pAdapter);
         // Inflate the layout for this fragment
+        populateData();
+
         return view;
+    }
+
+    private void populateData() {
+        PostInfo pi = new PostInfo("Goal1", "jimmy4", "Go home rn");
+        postList.add(pi);
+
+        pi = new PostInfo("Goal3", "jimmy17", "Go home NEBER");
+        postList.add(pi);
+
+        pi = new PostInfo("Habit22", "lennygoesHORT", "GO HORT");
+        postList.add(pi);
+
+        pAdapter.notifyDataSetChanged();
     }
 }
