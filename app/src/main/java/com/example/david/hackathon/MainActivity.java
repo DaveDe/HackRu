63a19e4309b196a4be3360952ddc0f98a0348258
David DeFazio <davedefazio1@gmail.com>
2016-04-17 00:41:55 -0400

    fixed goal box

--- app/src/main/java/com/example/david/hackathon/MainActivity.java
+++ app/src/main/java/com/example/david/hackathon/MainActivity.java
@@ -26,7 +26,6 @@ import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
-    //private Toolbar toolbar;
     private TabLayout tabLayout;
     private ViewPager viewPager;
 
